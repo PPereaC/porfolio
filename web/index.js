b705a49ea8fc39c4f784680e778434768937dad7
Pablo Perea Campos <74180013+PPereaC@users.noreply.github.com>
2024-12-31 18:36:31 +0100

    fix: Mostrar título bien

---
+++ web/index.js
@@ -0,0 +1,17 @@
+window.onload = function() {
+    // Mantener el título después de que Flutter cargue
+    const originalTitle = "Pablo Perea Campos - Porfolio";
+    
+    // Observar cambios en el título
+    const observer = new MutationObserver(function() {
+        if (document.title !== originalTitle) {
+            document.title = originalTitle;
+        }
+    });
+
+    observer.observe(document.querySelector('title'), { 
+        subtree: true, 
+        characterData: true, 
+        childList: true 
+    });
+};
