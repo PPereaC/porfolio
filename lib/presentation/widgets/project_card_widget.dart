ec87f0d74e84fb223cfa78b7b4828355b28ca03c
Pablo Perea Campos <74180013+PPereaC@users.noreply.github.com>
2025-01-01 19:18:04 +0100

    add: Navegar al repositiorio al pulsar un proyecto

--- lib/presentation/widgets/project_card_widget.dart
+++ lib/presentation/widgets/project_card_widget.dart
@@ -28,103 +28,110 @@ class _ProjectCardState extends State<ProjectCard> {
       return tag;
     }).toList();
 
-    return Center(
-      child: Container(
-        width: widget.isMobile ? screenWidth * 0.9 : screenWidth * 0.35,
-        height: widget.isMobile ? screenHeight * 0.6 : screenHeight * 0.5,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(16),
-          border: Border.all(
-            color: Colors.white.withOpacity(0.8),
-            width: 2,
-          ),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.white.withOpacity(0.5),
-              spreadRadius: 3,
-              blurRadius: 10,
-              offset: const Offset(0, 0),
+    return InkWell(
+      onTap: () async {
+        if (await canLaunchUrl(Uri.parse(widget.project.repositoryUrl))) {
+          await launchUrl(Uri.parse(widget.project.repositoryUrl));
+        }
+      },
+      child: Center(
+        child: Container(
+          width: widget.isMobile ? screenWidth * 0.9 : screenWidth * 0.35,
+          height: widget.isMobile ? screenHeight * 0.6 : screenHeight * 0.5,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(16),
+            border: Border.all(
+              color: Colors.white.withOpacity(0.8),
+              width: 2,
             ),
-          ],
-        ),
-        child: ClipRRect(
-          borderRadius: BorderRadius.circular(16),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              AspectRatio(
-                aspectRatio: 1.8,
-                child: Image.asset(
-                  widget.project.imagePath,
-                  fit: BoxFit.cover,
-                  width: double.infinity,
-                  height: widget.isMobile ? screenHeight * 0.2 : screenHeight * 0.3,
-                ),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.white.withOpacity(0.5),
+                spreadRadius: 3,
+                blurRadius: 10,
+                offset: const Offset(0, 0),
               ),
-              Expanded(
-                child: Container(
-                  color: Colors.black87.withOpacity(0.6),
-                  child: Padding(
-                    padding: widget.isMobile ? const EdgeInsets.all(12) : const EdgeInsets.all(16),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          widget.project.title,
-                          style: textTheme.titleMedium,
-                        ),
-                        Flexible(
-                          child: Text(
-                            widget.project.description,
-                            style: textTheme.bodyMedium!.copyWith(
-                              color: Colors.grey[400],
-                            ),
-                            overflow: TextOverflow.visible,
+            ],
+          ),
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(16),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                AspectRatio(
+                  aspectRatio: 1.8,
+                  child: Image.asset(
+                    widget.project.imagePath,
+                    fit: BoxFit.cover,
+                    width: double.infinity,
+                    height: widget.isMobile ? screenHeight * 0.2 : screenHeight * 0.3,
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    color: Colors.black87.withOpacity(0.6),
+                    child: Padding(
+                      padding: widget.isMobile ? const EdgeInsets.all(12) : const EdgeInsets.all(16),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            widget.project.title,
+                            style: textTheme.titleMedium,
                           ),
-                        ),
-                        SizedBox(height: screenHeight * 0.03),
-                        Wrap(
-                          spacing: 8,
-                          runSpacing: 8,
-                          children: tags.map((tag) => Container(
-                            padding: const EdgeInsets.symmetric(
-                              horizontal: 12,
-                              vertical: 6,
-                            ),
-                            decoration: BoxDecoration(
-                              color: colors.primary.withOpacity(0.1),
-                              borderRadius: BorderRadius.circular(8),
-                              border: Border.all(
-                                color: colors.primary.withOpacity(0.2),
-                                width: 1,
+                          Flexible(
+                            child: Text(
+                              widget.project.description,
+                              style: textTheme.bodyMedium!.copyWith(
+                                color: Colors.grey[400],
                               ),
+                              overflow: TextOverflow.visible,
                             ),
-                            child: Row(
-                              mainAxisSize: MainAxisSize.min,
-                              children: [
-                                Image.asset(
-                                  tag.imageUrl,
-                                  height: 16,
-                                  width: 16,
+                          ),
+                          SizedBox(height: screenHeight * 0.03),
+                          Wrap(
+                            spacing: 8,
+                            runSpacing: 8,
+                            children: tags.map((tag) => Container(
+                              padding: const EdgeInsets.symmetric(
+                                horizontal: 12,
+                                vertical: 6,
+                              ),
+                              decoration: BoxDecoration(
+                                color: colors.primary.withOpacity(0.1),
+                                borderRadius: BorderRadius.circular(8),
+                                border: Border.all(
+                                  color: colors.primary.withOpacity(0.2),
+                                  width: 1,
                                 ),
-                                const SizedBox(width: 8),
-                                Text(
-                                  tag.title,
-                                  style: textTheme.bodySmall?.copyWith(
-                                    color: colors.primary,
-                                    fontWeight: FontWeight.w500,
+                              ),
+                              child: Row(
+                                mainAxisSize: MainAxisSize.min,
+                                children: [
+                                  Image.asset(
+                                    tag.imageUrl,
+                                    height: 16,
+                                    width: 16,
                                   ),
-                                ),
-                              ],
-                            ),
-                          )).toList(),
-                        ),
-                      ],
+                                  const SizedBox(width: 8),
+                                  Text(
+                                    tag.title,
+                                    style: textTheme.bodySmall?.copyWith(
+                                      color: colors.primary,
+                                      fontWeight: FontWeight.w500,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            )).toList(),
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
