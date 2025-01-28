2caf4b5abe94a51e8eb49c3ed3de6eedfa8dac70
Pablo Perea Campos <74180013+PPereaC@users.noreply.github.com>
2025-01-28 21:12:04 +0100

    update: Varios Cambios (leer descripcion)
    - Cambiadas las carátulas de los proyectos.
    - Agregadas dos nuevas experiencias de trabajos.
    - Agregados dos nuevos proyectos.

--- lib/config/data/jobs_data.dart
+++ lib/config/data/jobs_data.dart
@@ -2,6 +2,22 @@ import '../../domain/entities/job.dart';
 
 List<Job> get jobs => [
   Job(
+    company: 'SUPERMERCADO LAS PALMERAS S.L',
+    position: 'Desarrollador de software',
+    period: 'oct. 2024 - nov. 2024 (2 meses) | Contrato por obra o servicio',
+    description: 'Desarrollé una aplicación de escritorio en Flutter para la gestión de productos, generando etiquetas con códigos de barras, nombre, cantidad, precio, y precio por KG/L para optimizar la organización y venta en el supermercado.',
+    technologies: ['Flutter', 'Microsoft Word', 'Isar Database'],
+    companyUrl: '',
+  ),
+  Job(
+    company: 'VICENTE NAVAS S.L',
+    position: 'Desarrollador de aplicaciones',
+    period: 'jul. 2024 - ago. 2024 (2 meses) | Contrato por obra o servicio',
+    description: 'Desarrollé una aplicación en Flutter para empleados, permitiendo gestionar y ver información de enfriadoras, con escaneo de QR para datos de máquinas físicas.',
+    technologies: ['Flutter', 'Firebase'],
+    companyUrl: 'https://www.vicentenavassl.com/',
+  ),
+  Job(
     company: 'DELTANET SISTEMAS DE INFORMACIÓN',
     position: 'Desarrollador de aplicaciones móviles y web',
     period: 'abr. 2024 - jun. 2024 (3 meses) | Prácticas en empresa',
--- lib/config/data/projects_data.dart
+++ lib/config/data/projects_data.dart
@@ -4,8 +4,22 @@ List<Project> get projects => [
   Project(
     title: 'CineRadar',
     description: 'Descubre películas y guarda tus favoritas en tu propia colección personalizada.',
-    imagePath: 'assets/projects/cineradar.png',
+    imagePath: 'assets/projects/cineradar.jpeg',
     tagLabels: ['flutter', 'dart', 'sqflite'],
     repositoryUrl: 'https://github.com/PPereaC/CineRadar'
-  )
+  ),
+  Project(
+    title: 'FinMusic',
+    description: 'Reproductor de música de YouTube Music sin necesidad de suscripción ni inicio de sesión.',
+    imagePath: 'assets/projects/finmusic.jpeg',
+    tagLabels: ['flutter', 'dart', 'sqflite'],
+    repositoryUrl: 'https://github.com/PPereaC/FinMusic'
+  ),
+  Project(
+    title: 'Porfolio',
+    description: 'Portfolio personal mostrando mis proyectos, habilidades y experiencia en desarrollo.',
+    imagePath: 'assets/projects/porfolio.jpeg',
+    tagLabels: ['flutter', 'dart'],
+    repositoryUrl: 'https://github.com/PPereaC/porfolio'
+  ),
 ];
